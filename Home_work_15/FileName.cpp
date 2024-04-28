2fc400d0d4b9d7e5f13f751ebe8cecc6d4e5ba3f
Mila8855 <Mila8855@outlook.com>
2024-04-28 18:53:25 +0300

    Добавьте файлы проекта.

---
+++ Home_work_15/FileName.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <algorithm>
+
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	int n;
+	const int rows = 10;
+	const int cols = 10;
+	int matrix[rows][cols]; // обьявление двумерного массива
+	srand(time(NULL));
+	int sum = 0;
+
+
+
+	std::cout << " \tИзначальный Массив:\n\t";
+
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < cols; j++) {
+			matrix[i][j] = rand() % (100 + 1); // [0;100] диапазон массива
+			std::cout << matrix[i][j] << ' ';
+
+		}
+		std::cout << "\n\t";
+	}
+	std::cout << "\n";
+
+	// Задача 1.
+
+	std::cout << " \tСумма каждого ряда:\n\t";
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < cols; j++) {
+			sum += matrix[i][j];
+			std::cout << matrix[i][j] << ' ';
+		}
+		std::cout << "\t" << sum << "\t";
+		std::cout << "\n\t";
+	}
+	std::cout << "\n\t";
+
+	// Задача 2.
+
+	std::cout << "Введите число от 0 до 9 -> ";
+	std::cin >> n;
+
+
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < cols; j++) {
+			if (i == n)
+				std::cout << matrix[i][j] << ' ';
+
+		}
+
+		std::cout << "\n\t";
+	}
+
+
+	return 0;
+
+}
